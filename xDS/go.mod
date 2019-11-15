b6a0df007334920f8664c89f98b1f2c1cfc13cfc
akaimo <kawashu0723@gmail.com>
2019-11-15 10:40:19 +0900

    add server

--- xDS/go.mod
+++ xDS/go.mod
@@ -3,6 +3,6 @@ module akaimo.com/envoy-sample/xds
 go 1.13
 
 require (
-	github.com/envoyproxy/go-control-plane v0.9.0 // indirect
-	google.golang.org/grpc v1.23.1 // indirect
+	github.com/envoyproxy/go-control-plane v0.9.0
+	google.golang.org/grpc v1.23.1
 )
---
+++ xDS/server/server.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"google.golang.org/grpc"
+	"net"
+
+	api "github.com/envoyproxy/go-control-plane/envoy/api/v2"
+	discovery "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v2"
+	"github.com/envoyproxy/go-control-plane/pkg/cache"
+	xds "github.com/envoyproxy/go-control-plane/pkg/server"
+)
+
+func main() {
+	snapshotCache := cache.NewSnapshotCache(false, cache.IDHash{}, nil)
+	server := xds.NewServer(snapshotCache, nil)
+	grpcServer := grpc.NewServer()
+	lis, _ := net.Listen("tcp", ":8080")
+
+	discovery.RegisterAggregatedDiscoveryServiceServer(grpcServer, server)
+	api.RegisterEndpointDiscoveryServiceServer(grpcServer, server)
+	api.RegisterClusterDiscoveryServiceServer(grpcServer, server)
+	api.RegisterRouteDiscoveryServiceServer(grpcServer, server)
+	api.RegisterListenerDiscoveryServiceServer(grpcServer, server)
+	go func() {
+		if err := grpcServer.Serve(lis); err != nil {
+			// error handling
+		}
+	}()
+}
